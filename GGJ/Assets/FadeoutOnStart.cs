1929ac21e49fb95964a14dc46cecff8c7dbf92b2
Emanuel Sharma <send.emanuel@hotmail.com>
2021-01-31 13:33:28 -0500

    Some UI stuff

---
+++ GGJ/Assets/FadeoutOnStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FadeoutOnStart : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    float mAlpha = 2f;
+
+    void Start()
+    {
+        StartCoroutine(FadeOutText(0.5f, text));
+    }
+
+    private IEnumerator FadeOutText(float timeSpeed, TextMeshProUGUI text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            mAlpha -= Time.deltaTime * timeSpeed;
+            text.color = new Color(text.color.r, text.color.g, text.color.b, mAlpha);
+            Debug.Log(mAlpha);
+            yield return null;
+        }
+    }
+}
